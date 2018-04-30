4ff3cdbe31379264bb09f4ee018c3fe9999b45b9
george <luiscarrillo129@gmail.com>
2018-04-29 17:18:24 -0700

    con error

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication2;
 
-/**
- *
- * @author GEORGE
- */
+
 public class JavaApplication2 {
 
     /**
@@ -16,6 +9,6 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    
     
 }
