120b1af002634911c07020cee751274232f1d7e2
george <luiscarrillo129@gmail.com>
2018-04-29 21:35:34 -0700

    proyecto maven

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NewMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author GEORGE
+ */
+public class NewMain {
+    public static void main(String[] args)
+    {
+        System.out.println("hola");
+    }
+    public int metodo(int i )
+    {
+    
+      return i+1;
+    }
+}
